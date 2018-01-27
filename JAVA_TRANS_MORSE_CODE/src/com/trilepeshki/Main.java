1846080066394669badc0496010b5ca73bcac876
AsetU1997 <97.infinity@mail.ru>
2018-01-27 14:17:45 +0300

    Morse CODE ENCRYPTER DECRYPTER WORKING

--- JAVA_TRANS_MORSE_CODE/src/com/trilepeshki/Main.java
+++ JAVA_TRANS_MORSE_CODE/src/com/trilepeshki/Main.java
@@ -1,8 +1,17 @@
 package com.trilepeshki;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String text= reader.readLine();
+        String morse = MorseTranslate.toMorze(text);
+        System.out.println(morse);
+        String str = MorseTranslate.toChar(morse);
+        System.out.println(str);
     }
 }
--- JAVA_TRANS_MORSE_CODE/src/com/trilepeshki/MorseTranslate.java
+++ JAVA_TRANS_MORSE_CODE/src/com/trilepeshki/MorseTranslate.java
@@ -1,34 +1,42 @@
 package com.trilepeshki;
 
+import com.sun.xml.internal.fastinfoset.util.CharArray;
+
 public class MorseTranslate {
-    protected static char[] characters = { 'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И',
+    protected static char[] characters = {
+            'А', 'Б', 'В', 'Г', 'Д', 'Е', 'Ж', 'З', 'И',
             'Й', 'К', 'Л', 'М', 'Н', 'О', 'П', 'Р', 'С',
-            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ы', 'Ь',
-            'Э', 'Ю', 'Я', '1', '2', '3', '4', '5', '6', '7',
-            '8', '9', '0' };
-    protected static String[] codeMorze = { "*–", "–***", "*––", "––*",
-            "–**", "*", "***–", "––**",
-            "**", "*–––", "–*–", "*–**",
-            "––", "–*", "–––", "*––*",
-            "*–*", "***", "–", "**–",
-            "**–*", "****", "–*–*",
-            "–––*", "––––", "−−*−",
-            "−*−−", "−**−", "**−**",
-            "**−−", "*−*−", "*−−−−",
-            "**−−−", "***−−", "****−",
-            "*****", "−****", "−−***",
-            "−−−**", "−−−−*", "−−−−−" };
-    public String toMorze (String text){
+            'Т', 'У', 'Ф', 'Х', 'Ц', 'Ч', 'Ш', 'Щ', 'Ъ',
+            'Ы', 'Ь', 'Э', 'Ю', 'Я',
+            '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', ' ' };
+    protected static String[] codeMorze = {
+            "*–", "–***", "*––", "––*", "–**", "*", "***–", "––**", "**",
+            "*–––", "–*–", "*–**", "––", "–*", "–––", "*––*", "*–*", "***",
+            "–", "**–", "**–*", "****", "–*–*", "–––*", "––––", "−−*−", "--*--",
+            "−*−−", "−**−", "**−**", "**−−", "*−*−",
+            "*−−−−", "**−−−", "***−−", "****−", "*****", "−****", "−−***",
+            "−−−**", "−−−−*", "−−−−−", "_" };
+
+    public static String toMorze (String text){
         String output = "";
         int index;
         char[] chars = text.toCharArray();
         for (char c : chars){
-            if (c != ' ')
-            {
-                index = text.indexOf(c);
+                index = new String(characters).indexOf(c);
                 output += codeMorze[index] + " ";
+        }
+       return output;
+    }
+    public static String toChar(String textM){
+        String input = "";
+        String[] str = textM.split(" ");
+        for(int i = 0; i < str.length; i++){
+            for(int j = 0; j < codeMorze.length; j++) {
+                if (str[i].equals(codeMorze[j])){
+                    input = input + characters[j];
+                }
             }
         }
-        return output;
+        return input;
     }
 }
