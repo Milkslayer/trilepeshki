cee03d98056fcc7a2c7e85c2da0ad07bbdf47ff0
Milkslayer <muhammed.bakijev@gmail.com>
2018-01-28 05:54:48 +0600

    update

--- UI_Chat/Flash/app/src/main/java/com/example/flash/CameraActivity.java
+++ UI_Chat/Flash/app/src/main/java/com/example/flash/CameraActivity.java
@@ -1,20 +1,40 @@
 package com.example.flash;
 
+import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.Canvas;
+import android.graphics.ImageFormat;
+import android.graphics.Rect;
+import android.graphics.YuvImage;
 import android.hardware.Camera;
+import android.net.Uri;
+import android.os.Environment;
+import android.renderscript.Allocation;
+import android.renderscript.Element;
+import android.renderscript.RenderScript;
+import android.renderscript.ScriptIntrinsicYuvToRGB;
+import android.renderscript.Type;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.FrameLayout;
+import android.widget.TextView;
 
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.OutputStream;
+import java.util.Date;
 
 import static android.R.attr.id;
+import static android.R.attr.width;
 import static android.provider.MediaStore.Files.FileColumns.MEDIA_TYPE_IMAGE;
 
 public class CameraActivity extends AppCompatActivity {
@@ -22,7 +42,6 @@ public class CameraActivity extends AppCompatActivity {
     private Camera camera;
     private CameraPreview mPreview;
     Camera.Parameters param;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -31,6 +50,9 @@ public class CameraActivity extends AppCompatActivity {
         camera = getCameraInstance();
         param = camera.getParameters();
         camera.setDisplayOrientation(90);
+        param.setColorEffect(android.hardware.Camera.Parameters.EFFECT_MONO);
+
+        camera.setParameters(param);
 
         mPreview = new CameraPreview(this, camera);
         FrameLayout preview = (FrameLayout) findViewById(R.id.camera_preview);
@@ -40,16 +62,82 @@ public class CameraActivity extends AppCompatActivity {
     public static Camera getCameraInstance(){
         Camera c = null;
         try {
-            c = Camera.open(); // attempt to get a Camera instance
+            c = Camera.open();
         }
         catch (Exception e){
-            // Camera is not available (in use or does not exist)
         }
-        return c; // returns null if camera is unavailable
+        return c;
     }
     public void backToMain(View v){
-        Intent i = new Intent(CameraActivity.this, MainActivity.class);
-        startActivity(i);
+        Bitmap b = screenShot(v);
+        save(b);
+    }
+    public Bitmap screenShot(View view) {
+        Bitmap bitmap = Bitmap.createBitmap(camera.getParameters().getPreviewSize().width,
+                camera.getParameters().getPreviewSize().width, Bitmap.Config.ARGB_8888);
+        Canvas canvas = new Canvas(bitmap);
+        view.draw(canvas);
+        return bitmap;
+    }
+    public void save(Bitmap b){
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+        TextView txt = (TextView) findViewById(R.id.textView2);
+        try {
+            AlertDialog.Builder alert = new AlertDialog.Builder(this);
+            alert.setMessage("Saved");
+            alert.show();
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/Pics/" + now + ".jpg";
+            txt.setText(mPath);
+            Bitmap bitmap = b;
+            File imageFile = new File(mPath);
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+
+            e.printStackTrace();
+        }
+    }
+
+    private void takeScreenshot(View v) {
+        Date now = new Date();
+        android.text.format.DateFormat.format("yyyy-MM-dd_hh:mm:ss", now);
+
+        try {
+
+            String mPath = Environment.getExternalStorageDirectory().toString() + "/" + now + ".jpg";
+
+
+            View v1 = getWindow().getDecorView().getRootView();
+            v1.setDrawingCacheEnabled(true);
+            Bitmap bitmap = Bitmap.createBitmap(v1.getDrawingCache());
+            v1.setDrawingCacheEnabled(false);
+
+            File imageFile = new File(mPath);
+
+            FileOutputStream outputStream = new FileOutputStream(imageFile);
+            int quality = 100;
+            bitmap.compress(Bitmap.CompressFormat.JPEG, quality, outputStream);
+            outputStream.flush();
+            outputStream.close();
+
+            openScreenshot(imageFile);
+        } catch (Throwable e) {
+
+            e.printStackTrace();
+        }
+    }
+    private void openScreenshot(File imageFile) {
+        Intent intent = new Intent();
+        intent.setAction(Intent.ACTION_VIEW);
+        Uri uri = Uri.fromFile(imageFile);
+        intent.setDataAndType(uri, "image/*");
+        startActivity(intent);
     }
 
 }
